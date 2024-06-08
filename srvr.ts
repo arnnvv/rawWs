69f32493e647f020ee18b5e0e676a0f266155b3b
arnnvv <arnav.900@protonmail.com>
2024-06-08 23:32:22 +0530

    pubSub-dn

--- srvr.ts
+++ srvr.ts
@@ -1,5 +1,6 @@
 import http from "http";
 import WebSocket, { RawData, WebSocketServer } from "ws";
+import RedisManager from "./redis";
 
 const port: number = 8080;
 
@@ -16,18 +17,27 @@ const users: {
 
 let count: number = 0;
 
-wss.on("connection", (ws: WebSocket) => {
+wss.on("connection", (ws: WebSocket): void => {
   const id = count++;
-  ws.on("message", (message: RawData) => {
+  ws.on("message", (message: RawData): void => {
     const data = JSON.parse(message.toString());
     if (data.type === "join") {
       users[id] = {
         roomId: data.payload.roomId,
         ws,
       };
+
+      RedisManager.getInstance().subscribe(
+        id.toString(),
+        data.payload.roomId,
+        ws,
+      );
     }
     if (data.type === "message") {
       const roomId = users[id].roomId;
+      RedisManager.getInstance().addChatMessage(roomId, message);
+
+      /*
       for (const userId in users) {
         if (users[userId].roomId === roomId) {
           users[userId].ws.send(
@@ -38,7 +48,12 @@ wss.on("connection", (ws: WebSocket) => {
           );
         }
       }
+      */
     }
+  });
+
+  ws.on("disconnect", () => {
+    RedisManager.getInstance().unsubscribe(id.toString(), users[id].roomId);
   });
 });
 
